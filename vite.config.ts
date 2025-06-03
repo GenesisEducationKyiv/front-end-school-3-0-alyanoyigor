09803ee7782afe7576cb7d6fc398c159902d335b
Igor Alyanoy <igoralyanoy@gmail.com>
2025-06-03 17:44:54 +0300

    fix: fix app local port

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import path from "path"
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    port: 3001,
   },
 });
