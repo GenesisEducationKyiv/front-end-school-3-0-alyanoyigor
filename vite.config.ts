7931afb4ca7f0bddd521bad168ba00764565b9d1
Igor Alyanoy <igoralyanoy@gmail.com>
2025-06-27 15:55:12 +0300

    feat: add bundle visualizer, split libs to chunks

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,34 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      plugins: [visualizer({ gzipSize: true, template: 'treemap' })],
+      output: {
+        manualChunks: {
+          vendor: [
+            'react',
+            'react-dom/client',
+            'react-router-dom',
+            'axios',
+            'zod',
+            '@tanstack/react-query',
+            'tailwind-merge',
+          ],
+          ui: [
+            '@radix-ui/react-dialog',
+            '@radix-ui/react-label',
+            '@radix-ui/react-popover',
+            '@radix-ui/react-progress',
+            '@radix-ui/react-select',
+            '@radix-ui/react-slot',
+            'sonner',
+          ],
+        },
+      },
     },
   },
   server: {
